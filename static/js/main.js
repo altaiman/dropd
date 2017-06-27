9ce8c21db815caf2c8b1d9837f3b917be0aedddf
altaiman <altaiman@gmail.com>
2017-06-27 22:31:49 +0700

    article

--- static/js/main.js
+++ static/js/main.js
@@ -42,3 +42,30 @@ $(function () {
         $('.search__btn_show').show();
     });
 });
+'use strict';
+
+$(function () {
+    $('[data-slider="article"]').flickity({
+        lazyLoad: true,
+        pageDots: false,
+        wrapAround: true,
+        arrowShape: {
+            x0: 10,
+            x1: 60, y1: 50,
+            x2: 70, y2: 45,
+            x3: 35
+        }
+    });
+
+    var sliderData = $('[data-slider="article"]').flickity().data('flickity'),
+        slides = sliderData.slides.length;
+    $('[data-slider="article"]').append('<span class="slider-count"></span>');
+
+    function slideCount() {
+        var currentSlide = sliderData.selectedIndex + 1;
+        $('.slider-count').html(currentSlide + '/' + slides);
+    }
+
+    slideCount();
+    $('[data-slider="article"]').on('select.flickity', slideCount);
+});
